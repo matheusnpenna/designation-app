ee5c70cda680d6eafc1aac153b467e060f372dd4
Matheus <matheus.penna@gerenciagram.com>
2019-10-11 17:37:04 -0300

    Add screen to workplace

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,7 +2,7 @@ import HomeScreen from './home';
 import LoginScreen from './login';
 import SignUpScreen from './signUp';
 import ResetPasswordScreen from './resetPassword';
-import WorkPlaceScreen from './workplace';
+import WorkPlaceScreen from './workPlace';
 
 export {
     HomeScreen,
