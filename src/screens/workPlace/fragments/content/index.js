7ce6b0192125fde7f3859b468ef23b6464f4a65b
Matheus <matheus.penna@gerenciagram.com>
2019-10-12 14:01:42 -0300

    Getting info of server and pass to selectable list

--- src/screens/workPlace/fragments/content/index.js
+++ src/screens/workPlace/fragments/content/index.js
@@ -3,6 +3,7 @@ import { TextInput, Text, View, Image, Keyboard } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { assets } from '../../../../config'
 import { SearchInput, SelectableList } from '../../../../components'
+import {  DesignationApi } from '../../../../services'
 import styles from './styles'
 
 class ContentFragment extends Component {
@@ -15,19 +16,37 @@ class ContentFragment extends Component {
         };
     }
 
+    onEndEditing = () => {
+        const { keyword } = this.state;
+        this.setState({ isLoading: true });
+        console.log(keyword);
+        DesignationApi.
+        getContentIdeas(keyword,
+            (contentList) => {
+                console.log(contentList);
+                this.setState({ contentList, isLoading: false });
+            });
+    }
+
     onChangeKeyword = (keyword) => this.setState({ keyword });
 
-    onSelectKeywordList = (contentIdeas) => this.setState({ contentIdeas });
-
+    onSelectKeywordList = (contentList) => this.setState({ contentList });
     
     render() {
         const { contentList } = this.state;
         return (
             <KeyboardAwareScrollView contentContainerStyle={styles.container}>
                 <Image source={assets.images.logoWithName} style={styles.img} />
-                <SearchInput onChange={this.onChangeKeyword} />
-                {(contentList.length > 0) && <Text style={styles.subtitle}>Pesquise por palavras-chave</Text>}
-                {(contentList.length > 0) && <SelectableList onSelectList={this.onSelectKeywordList} />}
+                <SearchInput onChange={this.onChangeKeyword} onEndEditing={this.onEndEditing} />
+                {(contentList.length < 0) &&
+                    <Text style={styles.subtitle}>
+                        Pesquise por palavras-chave
+                    </Text>}
+                {(contentList.length > 0) && 
+                    <SelectableList 
+                        onSelectList={this.onSelectKeywordList} 
+                        list={contentList}
+                    />}
             </KeyboardAwareScrollView>
         );
     }
