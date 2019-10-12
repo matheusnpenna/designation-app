1b133b1fb47fa39e410ba69354aa8b91744dcdbf
Matheus <matheus.penna@gerenciagram.com>
2019-10-12 14:01:26 -0300

    Better query string to build the url

--- src/services/designation-json.js
+++ src/services/designation-json.js
@@ -1,5 +1,5 @@
 const baseUrl = 'http://ec2-3-14-80-191.us-east-2.compute.amazonaws.com';
 
 export default {
-    contentIdeias: (keyword) => { return baseUrl + `/content_ideas/${keyword}`; },
+    contentIdeias: (keyword) => `${baseUrl}/content_ideas/${keyword}`
 }
