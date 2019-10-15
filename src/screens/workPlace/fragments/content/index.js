8c3b2ab8a38d40d83ae2af34536ae88bd32242a7
Matheus <matheus.penna@gerenciagram.com>
2019-10-14 22:42:38 -0300

    Add an button to generate layout

--- src/screens/workPlace/fragments/content/index.js
+++ src/screens/workPlace/fragments/content/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { TextInput, Text, View, Image, Keyboard } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { assets } from '../../../../config'
-import { SearchInput, SelectableList } from '../../../../components'
+import { SearchInput, SelectableList, LargeButton } from '../../../../components'
 import {  DesignationApi } from '../../../../services'
 import styles from './styles'
 
@@ -28,23 +28,32 @@ class ContentFragment extends Component {
 
     onChangeKeyword = (keyword) => this.setState({ keyword });
 
-    onSelectKeywordList = (contentList) => this.setState({ contentList });
+    onSelectKeywordList = (selectedList) => this.setState({ selectedList });
     
     render() {
         const { contentList } = this.state;
+
         return (
             <KeyboardAwareScrollView contentContainerStyle={styles.container}>
-                <Image source={assets.images.logoWithName} style={styles.img} />
+                {
+                    contentList.length <= 0 ? 
+                        <Image source={assets.images.logoWithName} style={styles.img} /> :
+                        <Text style={styles.title}>ESCOLHA OS MELHORES TEMAS</Text>
+                
+                }
                 <SearchInput onChange={this.onChangeKeyword} onEndEditing={this.onEndEditing} />
-                {(contentList.length < 0) &&
-                    <Text style={styles.subtitle}>
-                        Pesquise por palavras-chave
-                    </Text>}
+                {(contentList.length <= 0) && <Text style={styles.subtitle}>Pesquise por palavras-chave</Text>}
+                {(contentList.length > 0) && <SelectableList onSelectList={this.onSelectKeywordList} list={contentList} />}
                 {(contentList.length > 0) && 
-                    <SelectableList 
-                        onSelectList={this.onSelectKeywordList} 
-                        list={contentList}
-                    />}
+                    <View style={styles.submitButtonContainer}>
+                        <LargeButton
+                            containerCustomStyle={styles.buttonContainer}
+                            textCustomStyle={styles.buttonText}
+                            text={'Gerar Layout'}
+                            onPress={() => {}}
+                              />
+                    </View>
+                }
             </KeyboardAwareScrollView>
         );
     }
--- src/screens/workPlace/fragments/content/styles.js
+++ src/screens/workPlace/fragments/content/styles.js
@@ -8,11 +8,33 @@ export default {
         alignSelf: 'center',
         marginTop: 100
     },
+    title: {
+        color: colors.white,
+        fontSize: 14,
+        alignSelf: 'center',
+        marginTop: 20
+    },
     subtitle: {
         color: colors.white,
         fontSize: 14,
         alignSelf: 'center',
-        marginTop: 5,
-        fontWeight: 'bold',
+        marginTop: 5
+    },
+    submitButtonContainer: {
+        backgroundColor: colors.darkBlack,
+        padding: 30,
+        height: 100
+    },
+    buttonContainer: {
+        backgroundColor: colors.white,
+        width: '100%',
+        padding: 15,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: colors.darkBlack,
+        fontSize: 20
     }
 };
